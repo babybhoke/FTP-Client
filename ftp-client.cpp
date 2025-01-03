8f11328c75d5d83c9e8f013ca28f03f6bf1dccbd
babybhoke <bhokeboni@gmail.com>
2025-01-03 20:26:03 +0000

    created a socket

--- ftp-client.cpp
+++ ftp-client.cpp
@@ -11,5 +11,14 @@ private:
     std::string serverAddress;
     int serverPort;
 
-    bool connectToServer() {}
+    bool connectToServer() {
+        struct sockaddr_in serverAddr;
+
+        // Create socket
+        controlSocket = socket(AF_INET, SOCK_STREAM, 0);
+        if (controlSocket < 0) {
+            std::cerr << "Error creating socket!" << std::endl;
+            return false;
+        }
+    }
 };
